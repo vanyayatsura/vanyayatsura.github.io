a84fe02d0180af086a2f42fd5c4450c218b45a82
vanyayatsura <128183436+vanyayatsura@users.noreply.github.com>
2023-06-10 01:19:47 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
+
+
 const certificate = document.querySelector('#certificate')
 
+
 function openFoto() {
+  document.querySelector('body').style.overflow = 'hidden'
   certificate.style.opacity = "1"
   certificate.style.width = "100%"
   certificate.style.height = "100%"
 }
 function closeFoto() {
+  document.querySelector('body').style.overflow = 'auto'
   certificate.style.opacity = "0"
   certificate.style.width = "0%"
   certificate.style.height = "0%"
