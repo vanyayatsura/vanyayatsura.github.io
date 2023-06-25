d0c15bcec0edbec7de40a249a233d520676cccf7
Ivan Yatsura <vanyayacura@gmail.com>
2023-06-25 15:43:31 +0300

    corect clock about me and add reviesw

--- script/script.js
+++ script/script.js
@@ -1,17 +1,46 @@
 
 
 const certificate = document.querySelector('#certificate')
+const showReviews = document.querySelector('#showReviews')
 
 
 function openFoto() {
   document.querySelector('body').style.overflow = 'hidden'
-  certificate.style.opacity = "1"
-  certificate.style.width = "100%"
-  certificate.style.height = "100%"
+  
+  certificate.style.display = 'flex'
+  setTimeout(() => {
+    certificate.style.opacity = "1"
+    certificate.style.width = "100%"
+    certificate.style.height = "100%"
+  }, 200)
+  
 }
 function closeFoto() {
   document.querySelector('body').style.overflow = 'auto'
   certificate.style.opacity = "0"
   certificate.style.width = "0%"
   certificate.style.height = "0%"
+  setTimeout(() => {
+    certificate.style.display = 'none'
+  }, 200)
+}
+
+function openReviews() {
+  document.querySelector('body').style.overflow = 'hidden'
+  showReviews.style.display = 'flex'
+  setTimeout(() => {
+    showReviews.style.opacity = "1"
+    showReviews.style.width = "100%"
+    showReviews.style.height = "100%"
+  }, 200)
+}
+
+function closeReviews() {
+  document.querySelector('body').style.overflow = 'auto'
+  showReviews.style.opacity = "0"
+  showReviews.style.width = "0%"
+  showReviews.style.height = "0%"
+  setTimeout(() => {
+    showReviews.style.display = 'none'
+  }, 200)
 }
